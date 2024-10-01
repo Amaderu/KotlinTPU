eed53ae5c69508a12b662140d153546ccdf7faaa
Amaderu <ilahavelev454@gmail.com>
2024-10-02 02:59:43 +0700

    done all required fumctions

---
+++ lab2.kt
@@ -0,0 +1,191 @@
+/**
+ Задание
+Напишите программу, которая имитирует работу телефонной станции. В программе должно
+быть два класса:
+• Abonent – содержит поля с именем абонента и его номером, а также список входящих
+и исходящих звонков
+• Station – содержит список абонентов и два метода:
+o call(from: String, to: String) – ищет абонентов с именами from и to, и если
+они найдены, то первому добавляет в журнал вызовов строку «Исходящий к
+<…>», где вместо <…> подставляется имя абонента to, а второму добавляет в
+журнал вызовов строку «Входящий от <…>», где вместо <…> подставляется имя
+абонента from.
+o showStat() – выводит полные журналы звонков каждого абонента в формате:
+Журнал звонков абонента <…>:
+ Входящий от <…>
+ Входящий от <…>
+ Исходящий от <…>
+Журнал звонков абонента <…>:
+ Исходящий от <…>
+ Исходящий от <…>
+В функции main следует создать объект Station, добавить в него несколько абонентов,
+несколько раз вызвать функцию call() для имитации звонков между разными абонентами, а
+затем вызвать функцию showStat() для отображения журнала звонков.
+ */
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.TimeZone
+
+import kotlinx.datetime.format.*
+
+
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+
+
+
+enum class CallTypes{
+    INCOMING, OUTGOING
+}
+object Utils {
+    val DATE_TIME_PATTERN = LocalDateTime.Format {
+        date(
+            LocalDate.Format {
+            		dayOfMonth()
+					char(' ')
+                	monthName(MonthNames.ENGLISH_ABBREVIATED)
+                	chars(" ")
+                	year()
+            }
+        )
+        chars(" ., ")
+        time(
+            LocalTime.Format {
+                hour(); char(':'); minute()
+            }
+        )
+    }
+    
+    
+    
+    val PHONEPATTERN_GB = """/((\d{3})(?:\.|-))?(\d{3})(?:\.|-)(\d{4})/"""
+    val PHONEPATTERN_RU = """^(\+7|\d)\d{5,10}$"""
+    fun phoneCheker(phone: String): Boolean{
+        return if(Regex(PHONEPATTERN_RU).matchEntire(phone) != null) true else false
+    }
+}
+
+
+data class Call(var name : String, val number : String, val callType: CallTypes = CallTypes.OUTGOING){
+    val CallDate: LocalDateTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+    val CallTime: Duration = 0.seconds
+}
+
+class Abonent(){
+    private val id: UUID
+    	fun getId():UUID{return id}
+    private var name: String = ""
+//     	get(){return field}
+	fun getName():String {return name}
+    private var phoneNumber: String = ""
+//     	get(){return field}
+    fun getPhoneNumber():String {return phoneNumber}
+    
+    var calls: MutableList<Call> = mutableListOf()
+    	get() = field
+
+    init{
+        id = UUID.randomUUID()
+    }
+    
+    constructor(Name: String, PhoneNumber: String) : this(){
+        name = Name
+        phoneNumber = PhoneNumber
+//         phoneNumber = Utils.phoneCheker(value) ?: : 
+    }
+}
+
+
+class Station() {
+    var abonents: MutableList<Abonent>
+    fun call(from: String, to: String){
+        val contacts = abonents.filter{abonent -> abonent.getName() in listOf(from, to)}
+        //FIXME filter may find many contacts by @from@ number
+        val canCall = if ((contacts?.count() ?: 0) >= 2) true else false
+        
+        if(canCall){
+            val fromCall = contacts.find{it.getName() == from}
+            val toCall = contacts.find{it.getName() == to}
+            
+            
+            //FIXME preparing to chose correct contact
+            	fromCall?.calls?.add(
+                    Call(
+                        toCall!!.getName(), 
+                        toCall!!.getPhoneNumber(), 
+                        CallTypes.OUTGOING
+                    )
+                )
+                
+            	toCall?.calls?.add(
+            		Call(
+                        fromCall!!.getName(),
+                        fromCall!!.getPhoneNumber(),
+                        CallTypes.INCOMING
+                    )
+            	)
+            
+        }
+		
+    }
+        
+    fun print(msg: String) = println(msg)
+    
+    fun showStat(){
+        for(abonent: Abonent in abonents){
+            println("""Журнал звонков абонента <${abonent!!.getName()}>:""")
+            for(call: Call in abonent.calls){
+                when(call.callType){
+                    CallTypes.INCOMING -> this.print(""" Входящий от <${call!!.name}>""")
+                    CallTypes.OUTGOING -> this.print(""" Исходящий к <${call!!.name}>""")
+                    else -> {
+                        throw IllegalStateException()
+                    }
+            
+            	}
+            }
+        }
+    }
+    
+    init{
+        abonents = mutableListOf()
+    }
+    
+    constructor(Abonents: MutableList<Abonent>) : this(){
+       abonents = Abonents
+    }
+    
+} 
+
+// Журнал звонков абонента <…>:
+//  Входящий от <…>
+//  Входящий от <…>
+//  Исходящий от <…>
+// Журнал звонков абонента <…>:
+//  Исходящий от <…>
+//  Исходящий от <…>
+
+
+
+fun main() {
+// 	Create Abonents
+    val ivan = Abonent("ivan","+7-33-22-00")
+    val maria = Abonent("maria","+7-25-54-40")
+    val evgenyi = Abonent("evgenyi","+7-13-45-61")
+    
+//	Create station
+    val stationOne = Station(mutableListOf(ivan, maria, evgenyi))
+    stationOne.call(ivan.getName(), maria.getName())
+    
+    stationOne.call(evgenyi.getName(), maria.getName())
+    
+    stationOne.call(maria.getName(), ivan.getName())
+    stationOne.showStat()
+}
